4e23ae8333595fa5e12eb463f23224d5e3c11944
soo <kimmngsu@gamil.com>
2024-09-30 00:27:48 +0900

    깃 테스트

---
+++ Downloads/blog-toyproject/blog-toyproject/src/main/java/com/toyblog/blog_toyproject/BlogToyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.toyblog.blog_toyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogToyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogToyprojectApplication.class, args);
+	}
+
+}
---
+++ Downloads/blog-toyproject/blog-toyproject/src/test/java/com/toyblog/blog_toyproject/BlogToyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyblog.blog_toyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogToyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
