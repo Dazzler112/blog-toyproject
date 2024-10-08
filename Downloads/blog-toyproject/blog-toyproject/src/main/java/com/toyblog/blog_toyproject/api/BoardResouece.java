4147573a5e6ba6be57afb7bba8be425d253322fb
soo <kimmngsu@gamil.com>
2024-10-09 05:12:47 +0900

    [FEAT] about body insert 만드는중..

--- Downloads/blog-toyproject/blog-toyproject/src/main/java/com/toyblog/blog_toyproject/api/BoardResouece.java
+++ Downloads/blog-toyproject/blog-toyproject/src/main/java/com/toyblog/blog_toyproject/api/BoardResouece.java
@@ -3,6 +3,7 @@ package com.toyblog.blog_toyproject.api;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
+import com.toyblog.blog_toyproject.dto.*;
 import com.toyblog.blog_toyproject.service.*;
 
 @RestController
@@ -10,4 +11,19 @@ public class BoardResouece {
 
 	@Autowired
 	private BlogService blogService;
+	
+	@PostMapping("/about/{writer}/post")
+	public About writeAboutBody(@PathVariable String writer,
+				@RequestBody About about) {
+		
+		About writeAboutPost = blogService.addAbout(writer, about.getAbout_id(),
+					about.getBody(), about.getMember_id(), about.getTitle(),
+					about.getAbout_date());
+		
+		about.setBody(writer);
+		about.setMember_id(writer);
+		about.setWriter(writer);
+		
+		return writeAboutPost;
+	}
 }
--- Downloads/blog-toyproject/blog-toyproject/src/main/java/com/toyblog/blog_toyproject/mapper/BlogBoardMapper.java
+++ Downloads/blog-toyproject/blog-toyproject/src/main/java/com/toyblog/blog_toyproject/mapper/BlogBoardMapper.java
@@ -2,7 +2,32 @@ package com.toyblog.blog_toyproject.mapper;
 
 import org.apache.ibatis.annotations.*;
 
+import com.toyblog.blog_toyproject.dto.*;
+
 @Mapper
 public interface BlogBoardMapper {
+
+	@Insert("""
+			INSERT INTO 
+			ABOUT
+				(
+				  about_id
+				, title
+				, member_id
+				, writer
+				, body
+				, about_date
+				)
+			VALUES
+				(
+				    #{about_id}
+				  , #{title}
+				  , #{member_id}
+				  , #{writer}
+				  , #{body}
+				  , #{about_date}
+				 )
+			""")
+	About addAboutBody(About about);
 
 }
