46091da2e7734a1553264792d96d261a4041588c
soo <kimmngsu@gamil.com>
2024-10-22 00:04:05 +0900

    [UPDATE]About get Ajax로 전송 완료

--- Downloads/blog-toyproject/blog-toyproject/src/main/webapp/js/about/aboutget.js
+++ Downloads/blog-toyproject/blog-toyproject/src/main/webapp/js/about/aboutget.js
@@ -3,36 +3,31 @@ let aboutValue = "all";
 listAbout(aboutValue);
 
 function listAbout() {
-const memberId = "dummy";
-const aboutId = 1;
+	const memberId = "dummy";
+	const aboutId = 1;
 
-/*const requestMemberId = {
-	member_id : "dummy"
-};
-
-const requestAboutId = {
-	about_id : 1
-};
-
-
-const param1 = new URLSearchParams(requestMemberId).toString();
-const param2 = new URLSearchParams(requestAboutId).toString();
-*/
-$.ajax(`/about/${memberId}/post/${aboutId}`, {
-      method: "get",
-      contentType: "application/json",
-      complete: function(data) {
-		 const aboutList = data.aboutList;
-		 const abody = document.getElementById("about-body");
-		 abody.innerHTML= "";
-		 aboutList.forEach((about) => {
-			 const aboutHTML = `
-			 	<span style="display: none;">${about.about_id}</span>
-			 	<span style=" display: flex; justify-content: center; padding: 0px 200px; margin: 0px 80px;">${about.body}</span>
-			 `;
-			 abody.insertAdjacentHTML('beforeend', aboutHTML);
-		 });
-      }
-});
+	/*const requestMemberId = {
+		member_id : "dummy"
+	};
+	
+	const requestAboutId = {
+		about_id : 1
+	};
+	
+	
+	const param1 = new URLSearchParams(requestMemberId).toString();
+	const param2 = new URLSearchParams(requestAboutId).toString();
+	*/
+	$.ajax(`/about/${memberId}/post/${aboutId}`, {
+		method: "get",
+		contentType: "application/json",
+		success: function(result) { // 결과 성공 콜백함수
+			console.log(result.body);
+			$("#about-body").html(`<p>${result.body}</p>`);
+		},
+		error: function(request, status, error) { // 결과 에러 콜백함수
+			console.log(error)
+		}
+	});
 
 }
