be67e8c43d22f19f110688a2eb46df2c20d24c5e
soo <kimmngsu@gamil.com>
2024-11-13 22:35:33 +0900

    [FEAT]Mail searchMail 구현 메일중복 확인 로직

--- Downloads/blog-toyproject/blog-toyproject/src/main/java/com/toyblog/blog_toyproject/api/MailSendResource.java
+++ Downloads/blog-toyproject/blog-toyproject/src/main/java/com/toyblog/blog_toyproject/api/MailSendResource.java
@@ -3,6 +3,7 @@ package com.toyblog.blog_toyproject.api;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.security.core.*;
 import org.springframework.web.bind.annotation.*;
 
 import com.toyblog.blog_toyproject.service.*;
@@ -15,6 +16,8 @@ public class MailSendResource {
 	@Autowired
 	private MailSendService mailSendService;
 	
+	@Autowired
+	private BlogMemberService blogMemberService;
 	
 	@PostMapping("/mail/code")
 	public void CheckEmailCode(String email, HttpSession http) {
@@ -27,6 +30,12 @@ public class MailSendResource {
 		Boolean ok = mailSendService.compareNum(enteredCode, http);
 		
 		return Map.of("authentication", ok);
+	}
+	
+	@PostMapping("/searchmail")
+	public Map<String, Object> checkMailId(@RequestBody String email, Authentication authentication) {
+		
+		return blogMemberService.checkMailId(email, authentication);
 	}
 
 }
